85b16af4558e4dc8d5f7219709abedeb70af7bff
박힘찬 <himchan05@naver.com>
2022-03-06 17:13:38 +0900

    Initial Commit

---
+++ MyWebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWebView
+//
+//  Created by 박힘찬 on 2022/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWebView/MyWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWebViewApp.swift
+//  MyWebView
+//
+//  Created by 박힘찬 on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct MyWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
